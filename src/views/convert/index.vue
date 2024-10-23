dadf8872963d59868a936736d9bbc83697294597
ira james <irajames886@gmail.com>
2024-10-23 23:52:20 +0800

    updates: 组转直工具

--- src/views/convert/index.vue
+++ src/views/convert/index.vue
@@ -2,50 +2,72 @@
     <div class="main">
         <div class="top">
             <div>
-                <el-button type="primary">清空</el-button>
-                <el-button type="primary">计算</el-button>
-                <el-button type="primary">复制结果</el-button>
+                <el-button type="primary" @click="clearAll">清空</el-button>
+                <el-button type="primary" @click="doCalc">计算</el-button>
+                <el-button type="primary" @click="copyResult">复制结果</el-button>
             </div>
             <div>
                 <el-select v-model="currentMode" placeholder="选择模式">
-                <el-option v-for="(item,index) in modes" :key="index" :label="item" :value="item">
-                </el-option>
-            </el-select>
+                    <el-option v-for="(item, index) in modes" :key="index" :label="item" :value="item">
+                    </el-option>
+                </el-select>
             </div>
         </div>
         <div class="content">
             <el-card class="area">
                 <div slot="header">转直号码</div>
-                <div></div>
+                <div>
+                    <el-input v-model="directNums" placeholder="输入号码（空格分隔）"></el-input>
+                </div>
             </el-card>
             <el-card class="area">
                 <div slot="header">转直结果</div>
-                <div></div>
+                <div>
+                    <div class="result">{{ groupNums }}</div>
+                </div>
             </el-card>
         </div>
     </div>
 </template>
 
 <script>
+import { group2Direct } from "@/utils/code";
 export default {
     name: 'ConvertTool',
 
     data() {
         return {
-            modes: ["转直", "差集", "合集"],
-            currentMode: "转直",
-            directNums: [],
-            groupNums: [],
+            modes: ["组转直", "差集", "合集"],
+            currentMode: "组转直",
+            directNums: "",
+            groupNums: "",
             checkRules: []
         };
     },
-
-    mounted() {
-
+    computed: {
+        directArr() {
+            return this.directNums.trim().split(" ").filter(item => item,length > 0);
+        },
+        // groupResult(){
+        //     return this.groupNums.trim().split(" ").map(item => item.trim()).filter(item => item.length > 0)
+        // }
     },
 
     methods: {
+        clearAll() {
+            this.directNums = "";
+            this.groupNums = "";
+        },
+        doCalc() {
+            let groupArr = []
+            for (const code of this.directArr) {
+                groupArr = [...groupArr, ...group2Direct([...code])];
+            }
+            this.groupNums = [...groupArr].join(" ");
+        },
+        copyResult() {
 
+        }
     },
 };
 </script>
@@ -63,5 +85,10 @@ export default {
 
 .area {
     margin-bottom: 10px;
+}
+
+.result {
+    font-size: 14px;
+    color: #606266;
 }
 </style>
